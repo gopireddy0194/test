14c10e0686460d79cc22c3d9ac6993d02bf6c930
gopireddy0194 <gopireddyar1@gmail.com>
2021-05-14 08:37:46 +0000

    testfail commit

--- src/test/java/mypackage/CalculatorTest.java
+++ src/test/java/mypackage/CalculatorTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 public class CalculatorTest {
     @Test
     public void twoAndThreeIsFive() throws Exception {
-        final long result = new Calculator().addFucn(2, 3);
+        final long result = new Calculator().addFucn(2, 6);
         assertThat(result, is(5L));
     }
 
